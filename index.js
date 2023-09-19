120bc9ed340f236a81ca4a6bd12dda4158fdf17d
Kenneth <kennethlorenzg@gmail.com>
2023-09-18 20:33:21 -0400

    Add mouse hover/down and draw function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const gridContainer = document.querySelector("#gridContainer");
 const gridSize = 16;
-
+var grids;
 function createGrid() {
   for (let i = 0; i < gridSize * gridSize; i++) {
     const grid = document.createElement("div");
@@ -11,6 +11,34 @@ function createGrid() {
     grid.style.height = boxSize + "rem";
     gridContainer.appendChild(grid);
   }
+
+  grids = document.querySelectorAll(".grid");
 }
 
 createGrid();
+
+console.log(grids);
+
+function mouseHover() {
+  grids.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+      if (e.buttons == 1 || e.buttons == 3) {
+        item.classList.add("black");
+      }
+    });
+  });
+}
+function mouseDown() {
+  grids.forEach((item) => {
+    item.addEventListener("mousedown", () => {
+      item.classList.add("black");
+    });
+  });
+}
+
+function draw() {
+  mouseHover();
+  mouseDown();
+}
+
+draw();
